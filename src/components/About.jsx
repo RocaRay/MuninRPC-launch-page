707983fd80e57ec777e95b923fb040732026b319
Sterling Deng <sterlingdeng@gmail.com>
2019-04-17 15:35:16 -0700

    added gifs & changed file structure

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import "./_About.css";
+import React from 'react'
+import './_About.css'
+import clientStreamGif from '../assets/images/client_stream.gif'
+import bidiStreamGif from '../assets/images/bidi.gif'
+import serverStreamGif from '../assets/images/server_streaming.gif'
 
-const About = (props) => {
+const About = props => {
   return (
     <div className="about">
-    <h2>About</h2>
-      <p>MuninRPC: a GUI client for gRPC services.</p> 
+      <h2>About</h2>
+      <p>MuninRPC: a GUI client for gRPC services.</p>
       <p>Inspired by Postman and BloomRPC.</p>
       <p>Built on the shoulders of giants: protobuf.js & grpc-node.</p>
       <p>Mock requests to a gRPC service.</p>
-      <p>Upload your proto files, select a service, configure your message, and go.</p>
+      <p>
+        Upload your proto files, select a service, configure your message, and
+        go.
+      </p>
       <p>This is an early build. Development is in progress!</p>
 
       <h2>Features</h2>
       <ul>
-        <li>Supports unary, server-push, client-push, and bi-directional requests.</li>
+        <li>
+          Supports unary, server-push, client-push, and bi-directional requests.
+        </li>
         <li>Keeps a record of your outgoing and incoming messages.</li>
-        <li>Large proto file? No problem. Search your messages and services for what you need.</li>
+        <li>
+          Large proto file? No problem. Search your messages and services for
+          what you need.
+        </li>
         <li>Clean, beautiful, tabbed interface.</li>
       </ul>
+      <h2>Screen Grabs</h2>
+      <div className={'gif-box'}>
+        <img src={clientStreamGif} alt="client_stream_gif" />
+      </div>
+      <div className={'gif-box'}>
+        <img src={bidiStreamGif} alt="bid_stream_gif" />
+      </div>
+      <div className={'gif-box'}>
+        <img src={serverStreamGif} alt="server_stream_gif" />
+      </div>
     </div>
   )
 }
 
-export default About;
+export default About
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,41 @@
 import React from 'react'
-import "./_Header.css"
-import raven from ".././images/raven.png"
+import './_Header.css'
+import raven from '../assets/images/raven.png'
 
-const Header = (props) => {
+const Header = props => {
   return (
     <div className="header">
       <div className="header-left">
         <img className="raven" src={raven} alt="raven" />
       </div>
       <div className="header-right">
-        <div className={props.mode === "Home" ? "active" : ''} onClick={() => props.setMode("Home")}>Home</div>
-        <div className={props.mode === "About" ? "active" : ''} onClick={() => props.setMode("About")}>About</div>
-        <div className={props.mode === "Usage" ? "active" : ''} onClick={() => props.setMode("Usage")}>Usage</div>
-        <div className={props.mode === "Contact" ? "active" : ''} onClick={() => props.setMode("Contact")}>Contact Us</div>
+        <div
+          className={props.mode === 'Home' ? 'active' : ''}
+          onClick={() => props.setMode('Home')}
+        >
+          Home
+        </div>
+        <div
+          className={props.mode === 'About' ? 'active' : ''}
+          onClick={() => props.setMode('About')}
+        >
+          About
+        </div>
+        <div
+          className={props.mode === 'Usage' ? 'active' : ''}
+          onClick={() => props.setMode('Usage')}
+        >
+          Usage
+        </div>
+        <div
+          className={props.mode === 'Contact' ? 'active' : ''}
+          onClick={() => props.setMode('Contact')}
+        >
+          Contact Us
+        </div>
       </div>
     </div>
-    )
-  }
+  )
+}
 
 export default Header
